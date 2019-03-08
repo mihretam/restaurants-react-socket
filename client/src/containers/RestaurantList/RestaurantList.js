087764b6fd42db76c85b29edb4b785ccbed78f48
Mirza Hukic <mirza.hukic@serapion.net>
2019-03-08 12:50:52 +0100

    Changed state structure to a json format, the names of the restaurants are the keys and the values are json objects which contain the orders for the given restaurant and a bool property used to determine should the order window render or not

--- client/src/containers/RestaurantList/RestaurantList.js
+++ client/src/containers/RestaurantList/RestaurantList.js
@@ -6,30 +6,71 @@ import './RestaurantList.css'
 
 class RestaurantList extends Component {
     state = {
-        listOfRestaurants: [
-            "Zlatnik",
-            "Mozaik",
-            "NjamNjam",
-            "Sezam",
-            "Merak",
-            "Limenka",
-            "Old Story",
-            "Bagi"
-        ]
+        listOfRestaurants: {
+            "Zlatnik": {
+                orders: [],
+                openWindow: false
+            },
+
+            "Mozaik": {
+                orders: [],
+                openWindow: false
+            },
+
+            "NjamNjam": {
+                orders: [],
+                openWindow: false
+            },
+
+            "Sezam": {
+                orders: [],
+                openWindow: false
+            },
+
+            "Merak": {
+                orders: [],
+                openWindow: false
+            },
+
+            "Limenka": {
+                orders: [],
+                openWindow: false
+            },
+
+            "Old Story": {
+                orders: [],
+                openWindow: false
+            },
+
+            "Bagi": {
+                orders: [],
+                openWindow: false
+            }
+        }
     }
 
     componentDidUpdate(prevProps, prevState) {
         console.log(this);
     }
 
+    formatName = (newRestaurant) => {
+        return newRestaurant.charAt(0).toUpperCase() + newRestaurant.slice(1).toLowerCase();
+    }
+
     newRestaurantHandler = (newRestaurant) => {
-        const updatedListOfRestaurants = [...this.state.listOfRestaurants];
-        updatedListOfRestaurants.push(newRestaurant);
+        if (newRestaurant === "") {
+            return;
+        }
+        const updatedListOfRestaurants = {...this.state.listOfRestaurants};
+        updatedListOfRestaurants[newRestaurant] = {
+            orders: [],
+            openWindow: false
+        };
         this.setState({ listOfRestaurants: updatedListOfRestaurants });
     }
 
     render() {
-        let listOfRestaurants = this.state.listOfRestaurants.map(restaurantName => <RestaurantListItem key={restaurantName} restaurantName={restaurantName} />)
+        let listOfRestaurants = Object.keys(this.state.listOfRestaurants).map(restaurantName => <RestaurantListItem key={restaurantName} restaurantName={restaurantName} />)
         return (
             <div className="restaurantList">
                 <List component="ol">

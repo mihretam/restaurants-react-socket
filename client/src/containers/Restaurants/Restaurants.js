b5199afadeb94cab61cad979a9a131fe8ed8894b
mihretam <45687915+mihretam@users.noreply.github.com>
2019-03-13 15:31:39 +0100

    last changes

--- client/src/containers/Restaurants/Restaurants.js
+++ client/src/containers/Restaurants/Restaurants.js
@@ -2,36 +2,17 @@ import React, { Component } from 'react';
 
 import RestaurantList from '../../components/RestaurantList/RestaurantList';
 import NewRestaurantButton from '../../components/Button/NewRestaurantButton/NewRestaurantButton';
-<<<<<<< HEAD:client/src/containers/RestaurantList/RestaurantList.js
-import List from '@material-ui/core/List';
-//<<<<<<< HEAD
-import axios from 'axios';
-import Orders from '../../components/Orders/Orders'
-=======
 import Orders from '../../components/Orders/Orders';
->>>>>>> e12bb8a84e410c017dccb3e78585aaf9abd845cb:client/src/containers/Restaurants/Restaurants.js
 import axios from '../../services/RestaurantsAxios';
 import './Restaurants.css';
 
 class Restaurants extends Component {
     state = {
-<<<<<<< HEAD:client/src/containers/RestaurantList/RestaurantList.js
-        listOfRestaurants: {
-            
-        }
-    }
 
-    componentDidUpdate(prevProps, prevState) {
-        console.log(this);
-        listOfRestaurants: [],
-        orders: []
-//>>>>>>> 083b112c3822a505b76444164f7f6eb4dde88385
-=======
         listOfRestaurants: [
             {restaurantId: 2, restaurantName: 'asdas'}
         ],
         orders: [1, 2]
->>>>>>> e12bb8a84e410c017dccb3e78585aaf9abd845cb:client/src/containers/Restaurants/Restaurants.js
     }
 
     formatName = (newRestaurant) => {
